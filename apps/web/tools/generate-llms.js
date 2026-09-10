253dd284cfdefb169210c749e7e3882279f166a4
laerte77 <111751983+laerte77@users.noreply.github.com>
2026-09-10 15:38:31 -0300

    Update generate-llms.js

--- apps/web/tools/generate-llms.js
+++ apps/web/tools/generate-llms.js
@@ -91,7 +91,10 @@ function extractRoutes(appJsxPath) {
 }
 
 function findReactFiles(dir) {
-	return fs.readdirSync(dir).map(item => path.join(dir, item));
+    return fs.readdirSync(dir, { withFileTypes: true })
+        .filter(entry => entry.isFile())
+        .map(entry => path.join(dir, entry.name));
+}
 }
 
 function extractHelmetData(content, filePath, routes) {
