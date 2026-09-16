6fe7cf209b4c5d36229be330ac21f465ed74ed31
laerte77 <111751983+laerte77@users.noreply.github.com>
2026-09-16 15:57:54 -0300

    Update App.jsx

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -118,6 +118,7 @@ function App() {
                 
                 <Route path="*" element={<Navigate to="/" replace />} />
               </Routes>
+             </MobileOptimizedLayout>
           </ErrorBoundary>
         </SupabaseErrorBoundary>
       </div>
